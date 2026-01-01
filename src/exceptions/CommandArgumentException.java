83d5208c931bf563ad2cd28409b38b27a5c8b1d6
baguette <aaronhormann123@gmail.com>
2026-01-01 19:19:36 +0000

    empty rework commit

--- src/exceptions/CommandArgumentException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class CommandArgumentException extends CommandException {
-    public CommandArgumentException(String message) {
-        super("Illegal args: " + message);
-    }
-}
